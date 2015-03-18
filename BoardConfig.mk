6692a0b7b1bff71c3cf5d69d2f33647ea782ec53
bmc08gt <brandon.mcansh@gmail.com>
2015-03-18 20:06:10 +0100

    Update TARGET_ARCH* defines and kernel offsets
    Signed-off-by: bmc08gt <brandon.mcansh@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -3,17 +3,31 @@ USE_CAMERA_STUB := true
 # inherit from the proprietary version
 -include vendor/samsung/zerofltetmo/BoardConfigVendor.mk
 
-TARGET_ARCH := arm
-TARGET_NO_BOOTLOADER := true
-TARGET_BOARD_PLATFORM := unknown
-TARGET_CPU_ABI := armeabi
+TARGET_ARCH := arm64
+TARGET_ARCH_VARIANT := armv8-a
+TARGET_CPU_ABI := arm64-v8a
+TARGET_CPU_ABI2 :=
+TARGET_CPU_VARIANT := generic
+TARGET_CPU_SMP := true
+
+TARGET_2ND_ARCH := arm
+TARGET_2ND_ARCH_VARIANT := armv7-a-neon
+TARGET_2ND_CPU_ABI := armeabi-v7a
+TARGET_2ND_CPU_ABI2 := armeabi
+TARGET_2ND_CPU_VARIANT := generic
+
 TARGET_BOOTLOADER_BOARD_NAME := zerofltetmo
+
+TARGET_NO_BOOTLOADER := true
+TARGET_BOARD_PLATFORM := exynos7
 
 BOARD_KERNEL_CMDLINE := 
 BOARD_KERNEL_BASE := 0x10000000
+BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x01000000 --second_offset 0x10f00000 --tags_offset 0x00000100
 BOARD_KERNEL_PAGESIZE := 2048
 
 # fix this up by examining /proc/mtd on a running device
+TARGET_USERIMAGES_USE_EXt4 := true
 BOARD_BOOTIMAGE_PARTITION_SIZE := 0x00380000
 BOARD_RECOVERYIMAGE_PARTITION_SIZE := 0x00480000
 BOARD_SYSTEMIMAGE_PARTITION_SIZE := 0x08c60000
