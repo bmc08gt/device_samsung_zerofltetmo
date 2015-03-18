1625e3dd03f516d11c34a5e20ad91d7260fd477e
bmc08gt <brandon.mcansh@gmail.com>
2015-03-18 18:27:41 +0100

    AOKP-ify
    Signed-off-by: bmc08gt <brandon.mcansh@gmail.com>

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,2 +1,3 @@
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/device_zerofltetmo.mk
+    $(LOCAL_DIR)/full_zerofltetmo.mk \
+    $(LOCAL_DIR)/aokp.mk
---
+++ aokp.mk
@@ -0,0 +1,10 @@
+# Initialise device config
+$(call inherit-product, device/samsung/zerofltetmo/full_zerofltetmo.mk)
+
+# Inherit AOKP common Phone stuff.
+$(call inherit-product, vendor/aokp/configs/common.mk)
+
+PRODUCT_BUILD_PROP_OVERRIDES += BUILD_ID="LRX22G" BUILD_FINGERPRINT="samsung/zeroltetmo/zeroltetmo:5.0.2/LRX22G/G925TUVE1AOC5:eng/test-keys" PRIVATE_BUILD_DESC="zeroltetmo-eng 5.0.2 LRX22G G925TUVE1AOC5 test-keys"
+
+PRODUCT_NAME := aokp_zerofltetmo
+PRODUCT_DEVICE := zerofltetmo
