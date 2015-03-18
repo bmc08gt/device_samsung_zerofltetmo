693cd10c59ef8050e641dc01a5fbd0ad9b83c123
bmc08gt <brandon.mcansh@gmail.com>
2015-03-18 20:44:57 +0100

    Inherit from core_64_bit.mk
    Signed-off-by: bmc08gt <brandon.mcansh@gmail.com>

--- device.mk
+++ device.mk
@@ -17,8 +17,5 @@ endif
 PRODUCT_COPY_FILES += \
     $(LOCAL_KERNEL):kernel
 
-$(call inherit-product, build/target/product/full.mk)
-
-PRODUCT_BUILD_PROP_OVERRIDES += BUILD_UTC_DATE=0
 PRODUCT_NAME := full_zerofltetmo
 PRODUCT_DEVICE := zerofltetmo
--- full_zerofltetmo.mk
+++ full_zerofltetmo.mk
@@ -25,6 +25,10 @@
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
+PRODUCT_RUNTIMES := runtime_libart_default
+
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+
 # Inherit from zerfltetmo device
 $(call inherit-product, device/samsung/zerofltetmo/device.mk)
 
