8990fbf27d1e4ff716cee0140491273c7a6bf0f1
bmc08gt <brandon.mcansh@gmail.com>
2015-03-19 15:39:56 +0100

    Update build fingerprint and description
    Signed-off-by: bmc08gt <brandon.mcansh@gmail.com>

--- aokp.mk
+++ aokp.mk
@@ -7,7 +7,7 @@ TARGET_SCREEN_WIDTH := 1440
 # Inherit AOKP common Phone stuff.
 $(call inherit-product, vendor/aokp/configs/common.mk)
 
-PRODUCT_BUILD_PROP_OVERRIDES += BUILD_ID="LRX22G" BUILD_FINGERPRINT="samsung/zeroltetmo/zeroltetmo:5.0.2/LRX22G/G925TUVE1AOC5:eng/test-keys" PRIVATE_BUILD_DESC="zeroltetmo-eng 5.0.2 LRX22G G925TUVE1AOC5 test-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += BUILD_ID="LRX22G" BUILD_FINGERPRINT="samsung/zerofltetmo/zerofltetmo:5.0.2/LRX22G/G920TUVU1AOC9:user/release-keys" PRIVATE_BUILD_DESC="zerofltetmo-user 5.0.2 LRX22G G920TUVU1AOC9 release-keys"
 
 PRODUCT_NAME := aokp_zerofltetmo
 PRODUCT_DEVICE := zerofltetmo
