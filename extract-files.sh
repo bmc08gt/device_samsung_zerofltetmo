9d6c6b72966abcb0dd5217b15dd40cb553c0bc44
bmc08gt <brandon.mcansh@gmail.com>
2015-03-23 17:09:45 +0100

    Add initial proprietary blobs list and update scripts
    Signed-off-by: bmc08gt <brandon.mcansh@gmail.com>

--- extract-files.sh
+++ extract-files.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #
-# Copyright (C) 2013, 2015 The CyanogenMod Project
+# Copyright (C) 2013, 2014 The CyanogenMod Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -19,13 +19,12 @@
 #
 # https://github.com/poliva/random-scripts/blob/master/android/oat2dex.sh
 #
-# Support for arm-64 bit devices added by Brandon McAnsh
- 
+
 set -e
- 
+
 VENDOR=samsung
 DEVICE=zerofltetmo
- 
+
 if [ $# -eq 0 ]; then
   SRC=adb
 else
@@ -41,45 +40,54 @@ else
     exit 1
   fi
 fi
- 
- 
+
+
 oat2dex()
 {
     APK="$1"
- 
+
     OAT="`dirname $APK`/arm/`basename $APK .apk`.odex"
     if [ ! -e $OAT ]; then
-        OAT="`dirname $APK`/arm64/`basename $APK .apk`.odex"
-        if [ ! -e $OAT ]; then
-            return 0
-        fi
+        return 0
     fi
- 
+
     HIT=`r2 -q -c '/ dex\n035' "$OAT" 2>/dev/null | grep hit0_0 | awk '{print $1}'`
     if [ -z "$HIT" ]; then
         echo "ERROR: Can't find dex header of `basename $APK`"
         return 1
     fi
- 
+
     SIZE=`r2 -e scr.color=false -q -c "px 4 @$HIT+32" $OAT 2>/dev/null | tail -n 1 | awk '{print $2 $3}' | sed -e "s/^/0x/" | rax2 -e`
     r2 -q -c "pr $SIZE @$HIT > /tmp/classes.dex" "$OAT" 2>/dev/null
     if [ $? -ne 0 ]; then
         echo "ERROR: Something went wrong in `basename $APK`"
     fi
 }
- 
+
 BASE=../../../vendor/$VENDOR/$DEVICE/proprietary
 rm -rf $BASE/*
- 
- 
-for FILE in `cat proprietary-blobs.txt | grep -v ^# | grep -v ^$ | sed -e 's#^/system/##g' | sed -e "s#^-/system/##g"`; do
+
+
+for FILE in `egrep -v '(^#|^$)' proprietary-blobs.txt`; do
+    OLDIFS=$IFS IFS=":" PARSING_ARRAY=($FILE) IFS=$OLDIFS
+    FILE=${PARSING_ARRAY[0]}
+    DEST=${PARSING_ARRAY[1]}
+    if [ -z $DEST ]
+    then
+        DEST=$FILE
+    fi
     DIR=`dirname $FILE`
     if [ ! -d $BASE/$DIR ]; then
         mkdir -p $BASE/$DIR
     fi
- 
+
     if [ "$SRC" = "adb" ]; then
-        adb pull /system/$FILE $BASE/$FILE
+        adb pull /system/$FILE $BASE/$DEST
+        # if file does not exist try destination
+        if [ "$?" != "0" ]
+        then
+            adb pull /system/$DEST $BASE/$DEST
+        fi
         if [ "${FILE##*.}" = "apk" ]; then
             oat2dex /system/$FILE
         fi
@@ -89,12 +97,14 @@ for FILE in `cat proprietary-blobs.txt | grep -v ^# | grep -v ^$ | sed -e 's#^/s
             oat2dex $SRC/system/$FILE
         fi
     fi
- 
+
     if [ -e /tmp/classes.dex ]; then
         zip -gjq $BASE/$FILE /tmp/classes.dex
         rm /tmp/classes.dex
     fi
- 
+
 done
- 
+
 ./setup-makefiles.sh
+
+
