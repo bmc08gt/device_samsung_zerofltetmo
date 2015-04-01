e0b06690e5eab7661dda9aebacd337d732052d69
bmc08gt <brandon.mcansh@gmail.com>
2015-04-01 21:04:35 +0200

    Override ro.product.name
    Signed-off-by: bmc08gt <brandon.mcansh@gmail.com>

--- aokp.mk
+++ aokp.mk
@@ -7,7 +7,11 @@ TARGET_SCREEN_WIDTH := 1440
 # Inherit AOKP common Phone stuff.
 $(call inherit-product, vendor/aokp/configs/common.mk)
 
-PRODUCT_BUILD_PROP_OVERRIDES += BUILD_ID="LRX22G" BUILD_FINGERPRINT="samsung/zerofltetmo/zerofltetmo:5.0.2/LRX22G/G920TUVU1AOC9:user/release-keys" PRIVATE_BUILD_DESC="zerofltetmo-user 5.0.2 LRX22G G920TUVU1AOC9 release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRODUCT_NAME=zerofltetmo \
+    TARGET_DEVICE=zerofltetmo \
+    BUILD_FINGERPRINT="samsung/zerofltetmo/zerofltetmo:5.0.2/LRX22G/G920TUVU1AOC9:user/release-keys" \
+    PRIVATE_BUILD_DESC="zerofltetmo-user 5.0.2 LRX22G G920TUVU1AOC9 release-keys"
 
 PRODUCT_NAME := aokp_zerofltetmo
 PRODUCT_DEVICE := zerofltetmo
